cbdd4c49751ff08c7d2ae93afc49d8241acd9550
juraj.bacovcin <juraj.bacovcin@tatum.io>
2023-09-15 15:20:44 +0200

    init application

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# tatum-frontend-interview
+## Task Description
+
+1. Checkout the code from the repository
+
+2. Run the application locally
+
+```bash
+$ yarn install
+$ yarn dev
+```
+
+3. Debug the application and fix the bug (recommended address - `0x79d86cb2a28abb1489904555d73701221fdf4b85`)
+
+4. Review the code and suggest improvements
---
+++ components/molecules/Portfolio.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import { Token, Balances } from "@/lib/utils";
+
+import Card from "../atoms/Card";
+import Tab from "../atoms/Tab";
+
+const Portfolio = ({ balances }: { balances: Balances }): JSX.Element => {
+  const [activeTab, setActiveTab] = React.useState(1);
+
+  const handleTabChange = (tabNumber: number) => {
+    setActiveTab(tabNumber);
+  };
+
+  const TokenList = ({ items }: { items: Token[] }): JSX.Element => (
+    <div className="flex flex-col mt-4 text-gray-200 space-y-0 overflow-scroll max-h-40 divide-y">
+      {items[0] ? (
+        items.map((item, index) => (
+          <span className="p-4" key={index}>
+            <b>{item.label}</b>
+            <b className="float-right">{item.id ? ` (${item.id})` : ""}</b>
+            <p>{item.address}</p>
+          </span>
+        ))
+      ) : (
+        <i>No tokens</i>
+      )}
+    </div>
+  );
+
+  return (
+    <Card className="space-y-2">
+      <div className="flex pb-2">
+        <Tab
+          active={activeTab === 1}
+          text="Fungibles"
+          onClick={() => handleTabChange(1)}
+        />
+        <Tab
+          active={activeTab === 2}
+          text="NFTs"
+          onClick={() => handleTabChange(2)}
+        />
+        <Tab
+          active={activeTab === 3}
+          text="Multitokens"
+          onClick={() => handleTabChange(3)}
+        />
+      </div>
+
+      {activeTab === 1 && <TokenList items={balances.erc20} />}
+      {activeTab === 2 && <TokenList items={balances.erc721} />}
+      {activeTab === 3 && <TokenList items={balances.erc1155} />}
+    </Card>
+  );
+};
+
+export default Portfolio;
